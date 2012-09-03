3077196c6330bcb75128ee0a7af5188edeb8ba15
Matt Palmer <palmer.s.matt@gmail.com>
2012-09-03 15:32:20 -0400

    Flesh out Case and DeskSearchResult classes

--- desk.py
+++ desk.py
@@ -14,46 +14,22 @@ client = oauth.Client(consumer, token)
 # Date Tools {{{
 today = dt.datetime.today()
 
-def dtToYMD(datetimeObj):
+def dtStrToDtObj(date_str):
+    return dt.datetime.strptime(date_str, '%Y%m%d')
+
+def dtObjToYMD(datetimeObj):
     return datetimeObj.date().strftime('%Y%m%d')
 
-def datetimeToEpoch(datetimeObj):
+def dtObjToEpoch(datetimeObj):
     return str(int(time.mktime(datetimeObj.timetuple())))
 
-def timeRange(start_dt=None, end_dt=today, delta=None):
+def dateRange(start_dt=None, end_dt=today, delta=None):
     if start_dt == None and delta == None:
         raise NameError('Remember to specify either start_dt or delta.')
     elif not start_dt:
         start_dt = end_dt + dt.timedelta(days=delta)
-    return (datetimeToEpoch(start_dt), datetimeToEpoch(end_dt))
+    return (dtObjToEpoch(start_dt), dtObjToEpoch(end_dt))
 
-arg_hash = {
-        'today': timeRange(delta=0),
-        'yesterday': timeRange(delta=-1),
-        'week': timeRange(delta=-7),
-        'month': timeRange(delta=-30)
-        }
-# }}}
-# Classes {{{
-class CaseContainer:
-    def __init__(self, category, **params):
-        self.data = getFromDesk(category, **params)
-        self.count = self.data['count']
-        self.results = self.data['results']
-        self.total = self.data['total']
-        self.page = self.data['page']
-
-    def itercases(self):
-        pass
-        
-
-
-class Case:
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        pass
 # }}}
 # Functions {{{
 def getFromDesk(category, **params):
@@ -63,25 +39,54 @@ def getFromDesk(category, **params):
     for k, v in params.iteritems():
         request_url += '%s=%s&' % (k, v)
     res, content = client.request(request_url, "GET")
+    if not content:
+        return res
     return json.loads(content)
+# }}}
+# Classes {{{
+    # Case {{{
+class Case:
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __repr__(self):
+        pass
+    # }}}
+    # DeskSearchResult {{{
+class DeskSearchResult:
+    def __init__(self, **params):
+        self.data = getFromDesk('cases', **params)
+        self.count = self.data['count']
+        self.results = self.data['results']
+        self.total = self.data['total']
+        self.page = self.data['page']
+        self.cases = {}
+        for result in self.results:
+            caseId = result['case']['id']
+            self.cases[caseId] = Case(result['case'])
+
+    def __getitem__(self, index):
+        return self.cases[index]
+
+    def itercases(self):
+        for caseId in sorted(self.cases.keys()):
+            yield self.cases[caseId]
+    # }}}
 # }}}
 
 def main():
+    start, end = dateRange(delta=-3)
     params = {
-            'created': 'week',
-            'assigned_user': 'Patrick',
-            'count': '30'
+            'count': '100',
+            'status': 'new,open,pending',
+            'labels': 'needs',
+            'assigned_group': 'Customer'
             }
-
-    data = getFromDesk('cases', **params)
-
-    total = data['total']
-    results = data['results']
-
-    print data.keys()
-    print total
-    for result in results:
-        case = result['case']
+    box = DeskSearchResult(**params)
+    for case in box.itercases():
         print case['subject']
 
 if __name__ == '__main__':
