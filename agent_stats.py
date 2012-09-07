b60847a4f271d7d2ba350bda1efe205b1e10696e
Matt Palmer <palmer.s.matt@gmail.com>
2012-09-07 02:16:35 -0400

    add a rudimentary but working data storage system

--- agent_stats.py
+++ agent_stats.py
@@ -1,7 +1,8 @@
 #!usr/bin/env python
 
-import desk
 import cPickle as pickle
+import desk
+import os
 
 def weCanPickleThat(data, filename):
     out_f = open(filename, 'wb')
@@ -10,17 +11,36 @@ def weCanPickleThat(data, filename):
     finally:
         out_f.close()
 
+def weCanUnpickleThat(filename):
+    in_s = open(filename, 'rb')
+    try:
+        o = pickle.load(in_s)
+    finally:
+        in_s.close()
+    return o
+
+def getPickledCase(case_id, ext='.dat'):
+    case_id = str(case_id)
+    file_case_id = case_id
+    while len(file_case_id) < 5:
+        file_case_id = '0' + case_id
+    filename = file_case_id + ext
+    dirpath = '/'.join(list(str(case_id)))
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    filepath = '/'.join([dirpath, filename])
+    try:
+        case = weCanUnpickleThat(filepath)
+    except:
+        case = desk.Case(id_num=case_id)
+        weCanPickleThat(case, filepath)
+    return case
+
 def main():
-    search = desk.CaseSearch(
-            assigned_user='Matt Palmer',
-            status='open')
-
-    for case in search:
-        case.getInteractions()
-
-    case = search[-1]
-    weCanPickleThat(case, 'inter.dat')
-    weCanPickleThat(search, 'search.dat')
+    for i in range(3388, 3513):
+        case = getPickledCase(i)
+        if hasattr(case, 'subject'):
+            print case.subject
 
 if __name__ == '__main__':
     main()
