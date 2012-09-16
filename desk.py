86bdd538932611cf45ee8c7ea21bf9b631bce576
Matt Palmer <palmer.s.matt@gmail.com>
2012-09-16 16:56:40 -0400

    Testing for the cause of the blank case subjects.

--- desk.py
+++ desk.py
@@ -5,8 +5,9 @@ import desklib.classes   as cl
 import desklib.searches  as s
 
 def main():
-    search = cl.CaseSearch(**s.searches['unassigned'])
-    print len(search)
+    res, content = fn.getFromDesk('cases', s.searches['assigned'])
+    for k, v in content.iteritems():
+        pass
 
 
 if __name__ == '__main__':
