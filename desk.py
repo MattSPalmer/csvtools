b59f14d769762340dc5356acb0456d28edc1a89e
Matt Palmer <palmer.s.matt@gmail.com>
2012-09-18 00:28:42 -0400

    One step closer to a working Serena!

--- desk.py
+++ desk.py
@@ -1,21 +1,20 @@
 #!usr/bin/env python
 
 import desklib
+import datetime as dt
+from collections import Counter
 
-fn = desklib.functions
-cl = desklib.classes
-s  = desklib.searches
+fn   = desklib.functions
+cl   = desklib.classes
+pre  = desklib.presets
+
 
 def main():
-    params = {
-            'status': 'new,open,pending',
-            'assigned_user': 'Patrick'
-            }
-    search = cl.CaseSearch(**params)
-    print search
-    for case in search:
-        print case
-
+    search = cl.CaseSearch(**pre.searches['followup'])
+    statuses = Counter([c.user.name.split(' ')[0] for c in search if c.user])
+    for k, v in statuses.iteritems():
+        n = 'case' if v == 1 else 'cases'
+        fn.serenaSay(pre.serephra['updated'], name=k, noun=n, num=v)
 
 if __name__ == '__main__':
     main()
