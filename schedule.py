8ab4b776104f396d6f371f58ba62d3deca636712
Matt Palmer <palmer.s.matt@gmail.com>
2012-09-19 19:37:02 -0400

    removed shiftplanning submodule. I'll make my own, dammit.

--- schedule.py
+++ schedule.py
@@ -1,2 +1,24 @@
-from shiftplanning import sh_api
+import shiftplanning.shift_planning as sp
+from desklib.confidential import sp_creds
+from desklib.classes import DeskObject
 
+
+s = sp.ShiftPlanning(sp_creds['key'], sp_creds['username'],
+        sp_creds['password'])
+s.do_login()
+
+
+s.get_schedules()
+s.create_shift(
+        {
+            'start_time': '10:00 am',
+            'end_time': '11:00 pm',
+            'start_date': '10 November 2012',
+            'end_date': '10 November 2012',
+            'schedule': '82020'
+            }
+        )
+
+s.get_shifts()
+
+print s.get_public_data()
