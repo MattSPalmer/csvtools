46619a03dae2f87f75262adc3b16d34b7252276e
Matt Palmer <palmer.s.matt@gmail.com>
2012-09-23 19:49:15 -0400

    Restructure for single-use connections
    Easier for function-building.

--- schedule.py
+++ schedule.py
@@ -27,22 +27,46 @@ class AttributeRich(object):
     def __repr__(self):
         return str(self.data)
 
-connection = sp.ShiftPlanning(sp_creds['key'],
-sp_creds['username'], sp_creds['password'])
 
-connection.do_login()
-
-def onNow(daydiff=0):
-    now = dt.datetime.now() + dt.timedelta(days=daydiff)
-    connection.get_shifts(now, now, mode='overview')
+def get_shifts(start, end):
+    connection = sp.ShiftPlanning(sp_creds['key'], sp_creds['username'],
+            sp_creds['password'])
+    connection.do_login()
+    connection.get_shifts(start, end, mode='overview')
     shifts = map(AttributeRich, connection.get_public_data())
+    connection.do_logout()
+    del connection
+    return shifts
+
+def onNow():
+    now = dt.datetime.now()
+    shifts = get_shifts(now, now)
     employees = []
     for shift in shifts:
         if shift.start < now < shift.end:
             employees += [e.name for e in shift.employees]
+
     return employees
 
-print onNow()
+def onDay(num, hours=False):
+    date = dt.datetime.now() + dt.timedelta(days=num)
+    shifts = get_shifts(date, date)
+    employees = []
+    if hours == True:
+        for shift in shifts:
+            sched_str = "{} - {}".format(
+                    shift.start.strftime('%H:%M'),
+                    shift.end.strftime('%H:%M')
+                    )
+            delta = shift.end - shift.start
+            delta_hours = delta.days*24 + delta.seconds/3600
+            employees += [(e.name, sched_str, delta_hours) for e in shift.employees]
+    else:
+        for shift in shifts:
+            employees += [e.name for e in shift.employees]
 
-connection.do_logout()
+    return employees
+
+def isWorking(name):
+    return name in onNow()
 
